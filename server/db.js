80771ad66085a93f7c8996b42bdcee1c0cca7e85
Vbanti <olamuyiwavictor55@gmail.com>
2023-11-03 11:51:48 +0100

    added db configuration

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "victormania",
+    user: "moore",
+    password: "VCK8SpcMfWAm3wEjXBo4GZSyMWRZP7Yf",
     host:"localhost",
     port: 5432,
-    database: "moore"
+    database: "moore",
+    url:"postgres://moore:VCK8SpcMfWAm3wEjXBo4GZSyMWRZP7Yf@dpg-ckg00ugeksbs73de48ig-a.oregon-postgres.render.com/moore"
     
 });
 
