0028e24f82ef0912a1e487cd899a695aaa68631e
Vbanti <olamuyiwavictor55@gmail.com>
2023-11-05 00:20:28 +0100

    added db configurations

--- server/db.js
+++ server/db.js
@@ -2,10 +2,10 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool(
-    process.env.DB_URL,
+   
     {
     user: "root",
-    password: "VCK8SpcMfWAm3wEjXBo4GZSyMWRZP7Yf",
+    password: "BueoUo0Zfbdy3brSuoQ7QSpqG00FLxvS",
     host:"dpg-cl2dpobmgg9c73aqoshg-a",
     port: 5432,
     database: "moore_r5hq",
