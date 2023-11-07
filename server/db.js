9cbd4dfd81e9ec62c922b41d8769e5049495f2d3
Vbanti <olamuyiwavictor55@gmail.com>
2023-11-07 16:59:38 +0100

    added db configurations

--- server/db.js
+++ server/db.js
@@ -1,11 +1,18 @@
-const Pool = require("pg").Pool;
-require("dotenv").config();
+const postgres = require('postgres');
+require('dotenv').config();
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      require: true,
-    },
-  });
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const pool = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
 
 module.exports = pool;
