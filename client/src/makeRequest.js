3f95272194dc6b25383255385dda72a2f9ee2df8
Vbanti <olamuyiwavictor55@gmail.com>
2023-11-17 13:56:18 +0100

    added responsiveness

--- client/src/makeRequest.js
+++ client/src/makeRequest.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 
 
-const BASE_URL="http://localhost:5000/"
+const BASE_URL="https://mooreserver.onrender.com/"
 const RENDER_API="rnd_aNZ9enklIKwNgICV8oQiMktGR6aj"
 
 export const makeRequest = axios.create({
     baseURL : BASE_URL, 
-    headers: {
-        'Content-Type': 'application/json',
-    },
+    
     });
