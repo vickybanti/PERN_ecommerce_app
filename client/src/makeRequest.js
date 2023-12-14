84193d8ed9a4d39ba38c4b7cb27ad3b33336e47d
Vbanti <olamuyiwavictor55@gmail.com>
2023-12-14 10:57:08 +0100

    responsiveness

--- client/src/makeRequest.js
+++ client/src/makeRequest.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 
-//const BASE_URL="https://mooreserver.onrender.com/"
-const BASE_URL="http://localhost:5000/"
+const BASE_URL="https://mooreserver.onrender.com/"
 const RENDER_API="rnd_aNZ9enklIKwNgICV8oQiMktGR6aj"
 const headers = {
     'Content-Type': 'application/json',
