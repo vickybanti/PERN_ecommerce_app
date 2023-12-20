28322f76754afca9fc3da4611bf02b52d93cd5db
Vbanti <olamuyiwavictor55@gmail.com>
2023-12-20 14:46:10 +0100

    stripe checkout config

--- client/src/component/Pay/Payment.js
+++ client/src/component/Pay/Payment.js
@@ -25,7 +25,7 @@ function Payment({requestBody}) {
     fetch("https://mooreserver.onrender.com/checkout/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+      body: JSON.stringify(requestBody),
     })
       .then((res) => res.json())
       .then((data) => setClientSecret(data.clientSecret));
