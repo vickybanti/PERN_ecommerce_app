332e64baf50aeb71c790c3a5e1ac9037aea78f20
Vbanti <olamuyiwavictor55@gmail.com>
2024-01-10 13:34:47 +0100

    stripe config

--- client/src/pages/checkout/Checkout.js
+++ client/src/pages/checkout/Checkout.js
@@ -150,7 +150,7 @@ const newRequestBody = {
   setRequestBody(newRequestBody)
 
  
-    navigate("/payment?requestBody=" + encodeURIComponent(JSON.stringify(newRequestBody)));
+    navigate("/payment",{newRequestBody:newRequestBody});
     //navigate("/payment");
     setLoading(true)
 
