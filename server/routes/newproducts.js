7b51e90c4446fe972ab2ddee10eaec99557a4939
Vbanti <olamuyiwavictor55@gmail.com>
2024-02-18 02:39:14 +0100

    fixed cors

---
+++ server/routes/newproducts.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const pool = require("../db");
+
+router.get("/newproducts", async (req,res) => {
+    try {
+        const allProducts = await pool.query(`SELECT * FROM products WHERE type='newArrivals'`);
+        res.json(allProducts.rows);
+    } catch (err) {
+        console.log(err.message);
+        
+    }})
+
+
+module.exports=router
