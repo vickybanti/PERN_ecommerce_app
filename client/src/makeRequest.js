8247c1f4dfb938f6874b7b5c23110792014845e0
Vbanti <olamuyiwavictor55@gmail.com>
2024-02-19 21:05:03 +0100

    fixing cors

--- client/src/makeRequest.js
+++ client/src/makeRequest.js
@@ -6,8 +6,8 @@ const RENDER_API="rnd_aNZ9enklIKwNgICV8oQiMktGR6aj"
 const headers = {
     'Content-Type': 'application/json',
     'Authorization': 'Bearer (rnd_aNZ9enklIKwNgICV8oQiMktGR6aj)',
-    Origin: "https://mooreserver.onrender.com/",
-    "Access-Control-Allow-Origin": "*"
+    Origin: "https://pern-ecommerce-app.vercel.app/",
+    "Access-Control-Allow-Origin": "https://pern-ecommerce-app.vercel.app/"
 };
 
 
