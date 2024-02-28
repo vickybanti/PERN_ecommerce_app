6044c98346371065e4419e535149b564238d8465
Vbanti <olamuyiwavictor55@gmail.com>
2024-02-28 13:18:48 +0100

    fixing cors

--- client/src/makeRequest.js
+++ client/src/makeRequest.js
@@ -1,25 +1,20 @@
 import axios from "axios";
 
-
-const BASE_URL="https://mooreserver.onrender.com/"
-const RENDER_API="rnd_aNZ9enklIKwNgICV8oQiMktGR6aj"
+const BASE_URL = "https://mooreserver.onrender.com/";
+const RENDER_API = "rnd_aNZ9enklIKwNgICV8oQiMktGR6aj";
 const headers = {
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer (rnd_aNZ9enklIKwNgICV8oQiMktGR6aj)',
-    'origin':'https://pern-ecommerce-app.vercel.app',
-    'Access-control-allow-origin': 'https://mooreserver.onrender.com'
+  'Content-Type': 'application/json',
+  'Authorization': `Bearer ${RENDER_API}`,
+  'Access-Control-Allow-Origin': 'https://pern-ecommerce-app.vercel.app',
 };
 
-
 export const makeRequest = axios.create({
-    baseURL : BASE_URL, 
-    headers,
-    credentials: "include"
-    
-    });
+  baseURL: BASE_URL,
+  headers,
+  credentials: "include",
+});
 
 export const userRequest = axios.create({
-    baseURL : BASE_URL,
-    headers
-    
-})
+  baseURL: BASE_URL,
+  headers,
+});
