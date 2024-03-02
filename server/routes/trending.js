56c3e936ebcf00b24745fcaeabe43b8efcb7c820
Vbanti <olamuyiwavictor55@gmail.com>
2024-03-02 11:14:54 +0100

    fixing cors

--- server/routes/trending.js
+++ server/routes/trending.js
@@ -2,18 +2,11 @@
 const router = require("express").Router();
 const pool = require("../db");
 
-const cors = require("cors")
-
-router.use(cors({
-    "allowedHeaders":"*",
-    "origin":"*",
-        'Access-Control-Allow-Origin':"https://pern-ecommerce-app.vercel.app",
-    
-  }));
 
 
 
-router.get("/", cors(), async (req,res) => {
+
+router.get("/", async (req,res) => {
     try {
         const allProducts = await pool.query(`SELECT * FROM products WHERE type='trending'`);
         res.json(allProducts.rows);
