d49636846f3d4fa1dc17d23f303a50224682841d
Vbanti <olamuyiwavictor55@gmail.com>
2024-03-07 22:54:06 +0100

    stripe config

--- client/src/component/Pay/Payment.js
+++ client/src/component/Pay/Payment.js
@@ -1,53 +1,47 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from './CheckoutForm';
 
-import {Elements} from '@stripe/react-stripe-js';
-import CheckoutForm from './CheckoutForm'
-
-function Payment(props,requestBody) {
+function Payment(props, requestBody) {
   const { stripePromise } = props;
-  const [ clientSecret, setClientSecret ] = useState('');
+  const [clientSecret, setClientSecret] = useState('');
 
   useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
-   
-    
-    const pay = async()=> {
-      const response = await fetch("https://mooreserver.onrender.com/checkout/create-payment-intent",{
-      method:"POST",
-      body:JSON.stringify(requestBody),
-      headers:{"Content-Type":"application/json"}
-    })
+    const pay = async () => {
+      try {
+        const response = await fetch("https://mooreserver.onrender.com/checkout/create-payment-intent", {
+          method: "POST",
+          body: JSON.stringify(requestBody),
+          headers: { "Content-Type": "application/json" }
+        });
 
-    const getPay = await response.json()
-    console.log(getPay)
-    setClientSecret(getPay.clientSecret)
-    // .then((res) => console.log(res.json()))
-    // .then((data) => setClientSecret(data.clientSecret));
-  
-    }
-    pay()
+        const data = await response.json();
+        setClientSecret(data.client_secret); // Use data.client_secret instead of data.clientSecret
+      } catch (error) {
+        console.error("Error fetching client secret:", error.message);
+      }
+    };
+
+    pay();
   }, [requestBody]);
-
-  console.log(stripePromise)
-  console.log(clientSecret)
 
   const appearance = {
     theme: 'stripe',
   };
   const options = {
-    clientSecret,
+    clientSecret: `${clientSecret}`, // Use clientSecret as it is now in the correct format
     appearance,
   };
 
   return (
     <>
-    <div className='pay'>
-      <h1>Payment</h1>
-      {(
-        <Elements stripe={stripePromise} options={ options }>
-          <CheckoutForm />
-        </Elements>
-      )}
+      <div className='pay'>
+        <h1>Payment</h1>
+        {(
+          <Elements stripe={stripePromise} options={options}>
+            <CheckoutForm />
+          </Elements>
+        )}
       </div>
     </>
   );
