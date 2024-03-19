8e147369c7b798813f431837fa8c7bda58c37e2b
Vbanti <olamuyiwavictor55@gmail.com>
2024-03-19 13:11:34 +0100

    clientSecret

--- server/routes/checkout.js
+++ server/routes/checkout.js
@@ -118,7 +118,7 @@ router.get("/create-payment-intent", async function handlePaymentIntent (req, re
           console.log(paymentIntent.client_secret)
                
       
-        res.json({
+        res.send({
           publishableKey: "pk_test_51NDulnFA3ATF2zMuXsGjxz0JMzcX6Hj0QEQRBDx2RenNEnv3yz2R0WxB9cmSBhwrYzSMHago4LCa6nYPrSUkwBMu00Nx7VrwrY",
           client_secret: paymentIntent.id,
           // orders: createOrder.rows,
