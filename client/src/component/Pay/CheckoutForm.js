fec3281db5d0dd26d8880029e3ecd7566f8b766f
VICTOR <olamuyiwavictor55@gmail.com>
2024-06-10 22:41:09 +0100

    Stripe config

--- client/src/component/Pay/CheckoutForm.js
+++ client/src/component/Pay/CheckoutForm.js
@@ -1,21 +1,53 @@
+import React, { useEffect, useState } from "react";
 import {
     PaymentElement,
-    LinkAuthenticationElement
-} from '@stripe/react-stripe-js'
-import { useState } from 'react'
-import { useStripe, useElements } from '@stripe/react-stripe-js';
+    useStripe,
+    useElements
+} from "@stripe/react-stripe-js";
 
 export default function CheckoutForm() {
     const stripe = useStripe();
     const elements = useElements();
+
     const [message, setMessage] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if (!stripe) {
+            return;
+        }
+
+        const clientSecret = new URLSearchParams(window.location.search).get(
+            "payment_intent_client_secret"
+        );
+
+        if (!clientSecret) {
+            return;
+        }
+
+        stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+            switch (paymentIntent.status) {
+                case "succeeded":
+                    setMessage("Payment succeeded!");
+                    break;
+                case "processing":
+                    setMessage("Your payment is processing.");
+                    break;
+                case "requires_payment_method":
+                    setMessage("Your payment was not successful, please try again.");
+                    break;
+                default:
+                    setMessage("Something went wrong.");
+                    break;
+            }
+        });
+    }, [stripe]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
         if (!stripe || !elements) {
-            // Stripe.js has not yet loaded.
+            // Stripe.js hasn't yet loaded.
             // Make sure to disable form submission until Stripe.js has loaded.
             return;
         }
@@ -26,7 +58,7 @@ export default function CheckoutForm() {
             elements,
             confirmParams: {
                 // Make sure to change this to your payment completion page
-                return_url: `${window.location.origin}/completion`,
+                return_url: "http://localhost:3000",
             },
         });
 
@@ -38,24 +70,20 @@ export default function CheckoutForm() {
         if (error.type === "card_error" || error.type === "validation_error") {
             setMessage(error.message);
         } else {
-            setMessage("An unexpected error occured.");
+            setMessage("An unexpected error occurred.");
         }
 
         setIsLoading(false);
+    };
+
+    const paymentElementOptions = {
+        layout: "tabs"
     }
 
     return (
         <form id="payment-form" onSubmit={handleSubmit}>
-            <LinkAuthenticationElement id="link-authentication-element"
-            // Access the email value like so:
-            // onChange={(event) => {
-            //  setEmail(event.value.email);
-            // }}
-            //
-            // Prefill the email field like so:
-            // options={{defaultValues: {email: 'foo@bar.com'}}}
-            />
-            <PaymentElement id="payment-element" />
+
+            <PaymentElement id="payment-element" options={paymentElementOptions} />
             <button disabled={isLoading || !stripe || !elements} id="submit">
                 <span id="button-text">
                     {isLoading ? <div className="spinner" id="spinner"></div> : "Pay now"}
@@ -64,5 +92,5 @@ export default function CheckoutForm() {
             {/* Show any error or success messages */}
             {message && <div id="payment-message">{message}</div>}
         </form>
-    )
+    );
 }
--- client/src/component/Pay/Payment.js
+++ client/src/component/Pay/Payment.js
@@ -1,50 +1,45 @@
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
 
-import { Elements } from '@stripe/react-stripe-js';
-import CheckoutForm from './CheckoutForm'
+import CheckoutForm from "./CheckoutForm";
 
-function Payment(props) {
-    const { stripePromise } = props;
-    const [clientSecret, setClientSecret] = useState('');
-    console.log("stripepromis", stripePromise)
+// Make sure to call loadStripe outside of a component’s render to avoid
+// recreating the Stripe object on every render.
+// This is a public sample test API key.
+// Don’t submit any personally identifiable information in requests made with this key.
+// Sign in to see your own test API key embedded in code samples.
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
+
+export default function Payment() { 
+    const [clientSecret, setClientSecret] = useState("");
 
     useEffect(() => {
-        async function getSecret() {
-            try {
-                const response = await fetch("https://mooreserver.onrender.com/checkout/create_payment_intent", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    }
-                });
-                const data = await response.json();
-                const { clientSecret } = data; // Destructure clientSecret from the response data
-
-
-                setClientSecret(clientSecret);
-                console.log(clientSecret)
-            } catch (error) {
-                console.error("Error fetching secret:", error);
-            }
-        }
-
-        getSecret();
+        // Create PaymentIntent as soon as the page loads
+        fetch("https://mooreserver.onrender.com/checkout/create-payment-intent", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+        })
+            .then((res) => res.json())
+            .then((data) => setClientSecret(data.clientSecret));
     }, []);
 
-
+    const appearance = {
+        theme: 'stripe',
+    };
+    const options = {
+        clientSecret,
+        appearance,
+    };
 
     return (
-        <>
-            <div style={{paddingTop:"200px"} }>
-            <h1>Payment</h1>
-            {clientSecret && stripePromise && (
-                <Elements stripe={stripePromise} options={{ clientSecret }}>
+        <div style={{paddingTop:"200px"} }>
+            {clientSecret && (
+                <Elements options={options} stripe={stripePromise}>
                     <CheckoutForm />
                 </Elements>
             )}
-            </div>
-        </>
+        </div>
     );
 }
-
-export default Payment;
