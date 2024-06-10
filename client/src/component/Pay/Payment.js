21a70ce7c0817c32b90afeaf3d3d23c0835bedc5
VICTOR <olamuyiwavictor55@gmail.com>
2024-06-10 22:15:34 +0100

    Stripe config

--- client/src/component/Pay/Payment.js
+++ client/src/component/Pay/Payment.js
@@ -8,11 +8,26 @@ function Payment(props) {
     const [clientSecret, setClientSecret] = useState('');
 
     useEffect(() => {
-        // Create PaymentIntent as soon as the page loads
-        fetch("https://mooreserver.onrender.com/checkout/create_payment_intent")
-            .then((res) => res.json())
-            .then(({ clientSecret }) => setClientSecret(clientSecret));
+        async function getSecret() {
+            try {
+                const response = await fetch("https://mooreserver.onrender.com/checkout/create_payment_intent", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
+                const data = await response.json();
+                const { clientSecret } = data; // Destructure clientSecret from the response data
+
+                setClientSecret(clientSecret);
+            } catch (error) {
+                console.error("Error fetching secret:", error);
+            }
+        }
+
+        getSecret();
     }, []);
+
 
 
     return (
